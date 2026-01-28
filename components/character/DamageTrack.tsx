cbe920a63f92acb3efa452e296259c543ad2b587
Mavis Ryzhenko <mavistoriel@gmail.com>
2026-01-28 19:12:11 +0100

    feat: add icons for character header

--- components/character/DamageTrack.tsx
+++ components/character/DamageTrack.tsx
@@ -1,18 +1,26 @@
+import { Bone, Brain, Skull } from "lucide-react";
+
 export function DamageTrack() {
   return (
     <div className="flex flex-col">
       <div className="flex justify-between items-center">
-        <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">MD</div>
+        <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">
+          <Brain className="text-stone-900"/>
+        </div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">I</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">II</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">III</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">IV</div>
-        <div className="w-12 h-12 flex justify-center items-center border-2 border-stone-900">D</div>
+        <div className="w-12 h-12 flex justify-center items-center border-2 border-stone-900">
+          <Skull className="text-stone-900" />
+        </div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">IV</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">III</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">II</div>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">I</div>
-        <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">MD</div>
+        <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">
+          <Bone className="text-stone-900" />
+        </div>
       </div>
       <div className="flex justify-between">
         <span className="text-xs text-stone-500">mental damage</span>
--- components/character/ShockTrack.tsx
+++ components/character/ShockTrack.tsx
@@ -1,8 +1,12 @@
+import { HandFist } from "lucide-react";
+
 export function ShockTrack() {
   return (
     <div className="flex flex-col">
       <div className="flex justify-between items-center">
-        <div className="w-12 h-12 flex justify-center items-center border-2 border-stone-900">SHL</div>
+        <div className="w-12 h-12 flex justify-center items-center border-2 border-stone-900 rounded-xs">
+          <HandFist className="text-stone-900" />
+        </div>
         <hr className="border border-stone-900 w-1.5"/>
         <div className="w-9 h-9 flex justify-center items-center border-2 border-stone-900">1</div>
         <hr className="border border-stone-900 w-1.5"/>
