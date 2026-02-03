d7d4281d1991da33de1e5b4d9de84ab236300370
Mavis Ryzhenko <mavistoriel@gmail.com>
2026-02-03 16:22:48 +0100

    refactor: update UI components' props

--- components/ui/LabeledInput.tsx
+++ components/ui/LabeledInput.tsx
@@ -1,10 +1,11 @@
 import { InputHTMLAttributes } from 'react';
 import { cn } from '@/lib/utils';
 
-interface LabeledInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
+interface LabeledInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   variant?: 'title' | 'regular';
   containerClassName?: string;
+  error?: string;
 }
 
 export function LabeledInput({
@@ -12,22 +13,30 @@ export function LabeledInput({
   variant = 'regular',
   className,
   containerClassName,
+  error,
+  type = 'text',
   ...props
 }: LabeledInputProps) {
   return (
     <div className={cn('flex flex-col items-center gap-1', containerClassName)}>
       <input
-        type="text"
+        type={type}
         className={cn(
           'text-center border-b-2 border-stone-900 placeholder:text-stone-300 rounded-xs focus:bg-primary-50 active:border-primary-900 focus:outline-2 focus-visible:outline-2 focus:border-primary-900 outline-primary-600 outline-offset-0 focus:z-10',
           variant === 'title' && 'text-3xl font-bold min-w-96 w-96',
           variant === 'regular' && 'min-w-20 w-20',
+          error && 'border-red-500',
           className
         )}
         {...props}
       />
-      { label &&
-        <label className="text-xs text-stone-500 whitespace-nowrap text-nowrap">{label}</label>
+      { (label || error) &&
+        <label className={cn(
+          'text-xs whitespace-nowrap text-nowrap text-ellipsis',
+          error ? 'text-red-500' : 'text-stone-500'
+        )}>
+          {error || label}
+        </label>
       }
     </div>
   );
--- components/ui/RaisedButton.tsx
+++ components/ui/RaisedButton.tsx
@@ -1,15 +1,30 @@
 import { cn } from "@/lib/utils";
+import { ButtonHTMLAttributes } from "react";
 
-export function RaisedButton({ children, className, title }: { children?: React.ReactNode, className?: string, title?: string }) {
+export function RaisedButton({ 
+  children, 
+  className, 
+  title,
+  disabled,
+  onClick,
+  ...props 
+}: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button 
       title={title}
+      disabled={disabled}
+      onClick={onClick}
       className={cn(
-        "group mt-0.5 relative bg-image-sketch font-bold rounded-md cursor-pointer border-b border-stone-900",
+        "group mt-0.5 relative bg-image-sketch font-bold rounded-md border-b border-stone-900",
+        disabled ? "cursor-not-allowed opacity-60" : "cursor-pointer",
         className
       )}
+      {...props}
     >
-      <span className="relative -top-0.5 group-active:top-0 block uppercase bg-stone-50 p-2 rounded-md border-2 border-stone-900">
+      <span className={cn(
+        "relative -top-0.5 block uppercase bg-stone-50 p-2 rounded-md border-2 border-stone-900",
+        !disabled && "group-active:top-0"
+      )}>
         { children }
       </span>
     </button>
